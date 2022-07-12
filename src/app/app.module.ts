23aae6d60b0f961a2c58d519fe0a57e8c9022c4f
tiago-ferrini <amarikhalilmouh@gmail.com>
2022-07-12 11:22:45 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsListDefaultComponent } from './components/cards-list-default/cards-list-default.component';
 import { CardItemDefaultComponent } from './components/card-item-default/card-item-default.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [AppComponent, CardsListDefaultComponent, CardItemDefaultComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CardsListDefaultComponent,
+    CardItemDefaultComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
