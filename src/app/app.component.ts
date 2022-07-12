0e7dfd9a2e60f087ba8ca8eff1a74be5f71ee31b
tiago-ferrini <amarikhalilmouh@gmail.com>
2022-07-12 10:11:10 +0200

    add init airtable service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { AirtableService } from './services/airtable.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'yussuf';
+  constructor(private readonly airtableService: AirtableService) {}
 }
